f1e5f8741ddd0244a700fd90d29f3559f0b0ddd9
Wiji Fiko Teren <tobellord@gmail.com>
2022-12-06 19:30:39 +0700

    fix: skills item not shown

--- src/Screens/Home/Skills.jsx
+++ src/Screens/Home/Skills.jsx
@@ -18,14 +18,6 @@ import json from '../../Static/Images/Skills/json.png'
 import php from '../../Static/Images/Skills/php.png'
 
 export default function Skills() {
-    const [skills, setSkills] = useState([])
-
-    useEffect(() => {
-        return () => {
-            setSkills(getSkills)
-        }
-    }, [])
-
 
     return (
         <div className='skills row d-flex align-items-center justify-content-center'>
@@ -60,103 +52,101 @@ function Skill(args) {
 
 
 
-const getSkills = () => {
-    return [
-        {
-            name: 'React JS',
-            icon: reactjs,
-            url: 'https://reactjs.org',
-            color: '#57BDD9'
-        },
-        {
-            name: 'React Native',
-            icon: reactjs,
-            url: 'https://react-native.dev',
-            color: '#57BDD9'
-        },
-        {
-            name: 'JavaScript',
-            icon: javascript,
-            url: 'https://javascript.com',
-            color: '#FCDC00'
-        },
-        {
-            name: 'Node JS',
-            icon: nodejs,
-            url: 'https://nodejs.org',
-            color: '#5F975A'
-        },
-        {
-            name: 'CSS',
-            icon: css,
-            url: 'https://web.dev/learn/css',
-            color: '#2448D5'
-        },
-        {
-            name: 'Electron JS',
-            icon: electronjs,
-            url: 'https://electronjs.org',
-            color: '#8BC6D6'
-        },
-        {
-            name: 'SASS',
-            icon: sass,
-            url: 'https://sass-lang.com',
-            color: '#CC6699'
-        },
-        {
-            name: 'JQuery',
-            icon: jquery,
-            url: 'https://jquery.com',
-            color: '#1169AE'
-        },
-        {
-            name: 'MySQL DBMS',
-            icon: mysql,
-            url: 'https://mysql.com',
-            color: '#2299E1'
-        },
-        {
-            name: 'Bootstrap',
-            icon: bootstrap,
-            url: 'https://getbootstrap.com',
-            color: '#732FF8'
-        },
-        {
-            name: 'Automation using Chromium',
-            icon: chromechromium,
-            url: 'https://pptr.dev',
-            color: '#01D8A2'
-        },
-        {
-            name: 'Codeigniter',
-            icon: codeigniter,
-            url: 'https://codeigniter.com',
-            color: '#DD4814'
-        },
-        {
-            name: 'Github',
-            icon: github,
-            url: 'https://github.com',
-            color: '#fff'
-        },
-        {
-            name: 'HTML',
-            icon: html,
-            url: 'https://html.com',
-            color: '#8B4595'
-        },
-        {
-            name: 'Rest Full API',
-            icon: json,
-            url: 'https://restfulapi.net',
-            color: '#6D81B3'
-        },
-        {
-            name: 'PHP',
-            icon: php,
-            url: 'https://php.net',
-            color: '#7A86B8'
-        }
-    ]
-}
+const skills = [
+    {
+        name: 'React JS',
+        icon: reactjs,
+        url: 'https://reactjs.org',
+        color: '#57BDD9'
+    },
+    {
+        name: 'React Native',
+        icon: reactjs,
+        url: 'https://react-native.dev',
+        color: '#57BDD9'
+    },
+    {
+        name: 'JavaScript',
+        icon: javascript,
+        url: 'https://javascript.com',
+        color: '#FCDC00'
+    },
+    {
+        name: 'Node JS',
+        icon: nodejs,
+        url: 'https://nodejs.org',
+        color: '#5F975A'
+    },
+    {
+        name: 'CSS',
+        icon: css,
+        url: 'https://web.dev/learn/css',
+        color: '#2448D5'
+    },
+    {
+        name: 'Electron JS',
+        icon: electronjs,
+        url: 'https://electronjs.org',
+        color: '#8BC6D6'
+    },
+    {
+        name: 'SASS',
+        icon: sass,
+        url: 'https://sass-lang.com',
+        color: '#CC6699'
+    },
+    {
+        name: 'JQuery',
+        icon: jquery,
+        url: 'https://jquery.com',
+        color: '#1169AE'
+    },
+    {
+        name: 'MySQL DBMS',
+        icon: mysql,
+        url: 'https://mysql.com',
+        color: '#2299E1'
+    },
+    {
+        name: 'Bootstrap',
+        icon: bootstrap,
+        url: 'https://getbootstrap.com',
+        color: '#732FF8'
+    },
+    {
+        name: 'Automation using Chromium',
+        icon: chromechromium,
+        url: 'https://pptr.dev',
+        color: '#01D8A2'
+    },
+    {
+        name: 'Codeigniter',
+        icon: codeigniter,
+        url: 'https://codeigniter.com',
+        color: '#DD4814'
+    },
+    {
+        name: 'Github',
+        icon: github,
+        url: 'https://github.com',
+        color: '#fff'
+    },
+    {
+        name: 'HTML',
+        icon: html,
+        url: 'https://html.com',
+        color: '#8B4595'
+    },
+    {
+        name: 'Rest Full API',
+        icon: json,
+        url: 'https://restfulapi.net',
+        color: '#6D81B3'
+    },
+    {
+        name: 'PHP',
+        icon: php,
+        url: 'https://php.net',
+        color: '#7A86B8'
+    }
+]
